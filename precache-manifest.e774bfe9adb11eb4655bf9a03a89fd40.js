d462f3075c1f281b392bdd8b3b245e850909eca3
solomonbarayev <sbarayev2@gmail.com>
2022-07-20 10:14:40 +0300

    Updates

---
+++ precache-manifest.e774bfe9adb11eb4655bf9a03a89fd40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87556da4d16024420a7959b7597a6a6",
+    "url": "/react-tabs-component/index.html"
+  },
+  {
+    "revision": "a661c7655c297ebad319",
+    "url": "/react-tabs-component/static/css/main.bd4f4912.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/react-tabs-component/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tabs-component/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a661c7655c297ebad319",
+    "url": "/react-tabs-component/static/js/main.e2f35e21.chunk.js"
+  },
+  {
+    "revision": "a0a1203ba660fb368e44",
+    "url": "/react-tabs-component/static/js/runtime-main.ee010aa2.js"
+  }
+]);
